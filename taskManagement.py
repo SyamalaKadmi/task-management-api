d4d45a85fa5e20672dd8aa50c08225275602274c
abdulqudduspc <abdulqudduspc@outlook.com>
2024-08-17 15:41:13 +0530

    Retrieve GET method added

--- taskManagement.py
+++ taskManagement.py
@@ -7,6 +7,17 @@ tasks = [ {"id": 1, "title": "AdminService.exe", "description": "Windows Setup A
              {"id": 3, "title": "LockApp.exe", "description": "LockApp.exe", "status": "Suspended", "created_at":"17-08-2024", "updated_at":"17-08-2024"},
                ]
 
+# GET /tasks: Retrieve all tasks
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+# GET /tasks/<id>: Retrieve a specific task
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    task = next(task for task in tasks if task['id'] == id)
+    return jsonify(task)
+
 if __name__=='__main__':
 
     app.run(debug=True)
