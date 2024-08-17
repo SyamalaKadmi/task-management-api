053a2eb56f428faf8af998c8a04e6b30125dc556
Syamala <syamala.kadimi@gmail.com>
2024-08-17 15:24:48 +0530

    initial api file check-in

---
+++ taskManagement.py
@@ -0,0 +1,6 @@
+# #GET /tasks: Retrieve all tasks
+# GET /tasks/<id>: Retrieve a specific task
+# POST /tasks: Create a new task
+# PUT /tasks/<id>: Update an existing task
+# DELETE /tasks/<id>: Delete a task
+
