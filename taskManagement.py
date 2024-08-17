5da221f3f9a0b1b5b313d2419159c4c4c5c6e2a4
Syamala <syamala.kadimi@gmail.com>
2024-08-17 15:46:42 +0530

    Put api updated

--- taskManagement.py
+++ taskManagement.py
@@ -4,3 +4,22 @@
 # PUT /tasks/<id>: Update an existing task
 # DELETE /tasks/<id>: Delete a task
 
+from flask import Flask, jsonify, request
+
+app=Flask(__name__)
+
+tasks = [ {"id": 1, "title": "AdminService.exe", "description": "Windows Setup API", "status": "Running", "created_at":"17-08-2024", "updated_at":"17-08-2024"},
+             {"id": 2, "title": "conhost.exe", "description": "Console Windows Host", "status": "Running", "created_at":"17-08-2024", "updated_at":"17-08-2024"}, 
+             {"id": 3, "title": "LockApp.exe", "description": "LockApp.exe", "status": "Suspended", "created_at":"17-08-2024", "updated_at":"17-08-2024"},
+               ]
+
+@app.route("/tasks/<id>", methods=["PUT"])
+def task_update(id):
+    tasks = tasks.query.get(id)
+    title = request.json['title']
+    description = request.json['description']
+    return("Task has been updated")
+
+if __name__=='__main__':
+
+    app.run(debug=True)
