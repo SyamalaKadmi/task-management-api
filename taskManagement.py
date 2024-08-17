d2b760a6eed3f48a3d034961bca95c2f70881b81
SyamalaKadmi <166597892+SyamalaKadmi@users.noreply.github.com>
2024-08-17 15:35:59 +0530

    Update taskManagement.py

--- taskManagement.py
+++ taskManagement.py
@@ -1,6 +1,12 @@
-# #GET /tasks: Retrieve all tasks
-# GET /tasks/<id>: Retrieve a specific task
-# POST /tasks: Create a new task
-# PUT /tasks/<id>: Update an existing task
-# DELETE /tasks/<id>: Delete a task
+from flask import Flask, jsonify, request
 
+app=Flask(__name__)
+
+tasks = [ {"id": 1, "title": "AdminService.exe", "description": "Windows Setup API", "status": "Running", "created_at":"17-08-2024", "updated_at":"17-08-2024"},
+             {"id": 2, "title": "conhost.exe", "description": "Console Windows Host", "status": "Running", "created_at":"17-08-2024", "updated_at":"17-08-2024"}, 
+             {"id": 3, "title": "LockApp.exe", "description": "LockApp.exe", "status": "Suspended", "created_at":"17-08-2024", "updated_at":"17-08-2024"},
+               ]
+
+if __name__=='__main__':
+
+    app.run(debug=True)
